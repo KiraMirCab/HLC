617fbec952ed90c08125a19f665a713e9acd5822
Kira <kira.mironova.external@atos.net>
2021-10-02 14:05:11 +0200

    Terminadas las tarjetas de articulos

--- src/app/components/articulos/articulos.component.ts
+++ src/app/components/articulos/articulos.component.ts
@@ -14,29 +14,28 @@ export class ArticulosComponent implements OnInit {
       precio: 19,
       descuento: true,
       descripcion: "Un comic para los pequeños, aunque los padres también disfrutarán de su sentido de humor.",
+      img: "../../assets/Images/SuperPatata.jpg"
     },
     {
       nombre: "Super Patata 2",
       precio: 20,
       descuento: true,
       descripcion: "Segunda parte del famoso comic para niños.",
-      img: "../../assets/Images/SuperPatata.jpg"
+      img: "../../assets/Images/SuperPatata2.jpg"
     },
     {
       nombre: "Pinoccio",
       precio: 35,
       descuento: false,
-      descripcion: "La historia que se leyó caso cada adulto cuando era niño."
+      descripcion: "La historia que se leyó casi cada adulto cuando era niño."
 
     },
     {
       nombre: "Guerra y Paz",
       precio: 105.45,
       descuento: true,
-      descripcion: "Novela épica de amor y de querra, mejor que Game of Thrones sin duda. "
+      descripcion: "Novela épica de amor y de querra de 1812, mejor que Game of Thrones sin duda. "
     },
- 
- 
   ]
   constructor() { }
 
